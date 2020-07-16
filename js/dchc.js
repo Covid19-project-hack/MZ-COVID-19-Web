874835f80eed68d572b735e963269bef0b9bb17a
Sandykum <ssannthoshkumar@gmail.com>
2020-07-17 01:44:53 +0530

    master

--- js/dchc.js
+++ js/dchc.js
@@ -39,7 +39,7 @@ $(document).ready(function(){
     return json;
   })();
 
-tollnums =  testlabs.dchc;
+tollnums =  testlabs.dchcc;
 tollnums.forEach(element => { 
 listgrp.innerHTML+=
 `<div class="list-group-item list-group-item-action flex-column align-items-start " style="padding-bottom: 20px;">
